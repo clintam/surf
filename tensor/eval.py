aa857d4193b9628a7a2796e218b8bdb4c22a8ff1
Clint Morgan <clint@rekener.com>
2017-01-03 16:27:55 -0800

    Rework tensor scripts to re-useable abstractions
    And add minimal tests

--- tensor/eval.py
+++ tensor/eval.py
@@ -1,69 +1,13 @@
 #! /usr/bin/env python
 
-import tensorflow as tf
-import numpy as np
-import os
-import time
-import datetime
-import data_helpers
-from text_cnn import TextCNN
-from tensorflow.contrib import learn
-import csv
+import predictor
 
-# Parameters
-# ==================================================
+prediction = predictor.Predictor()
 
-# Data Parameters
-tf.flags.DEFINE_string("positive_data_file", "./data/rt-polaritydata/rt-polarity.pos", "Data source for the positive data.")
-tf.flags.DEFINE_string("negative_data_file", "./data/rt-polaritydata/rt-polarity.neg", "Data source for the positive data.")
-
-# Eval Parameters
-tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
-tf.flags.DEFINE_string("checkpoint_dir", "", "Checkpoint directory from training run")
-tf.flags.DEFINE_boolean("eval_train", False, "Evaluate on all training data")
-
-# Misc Parameters
-tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
-tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
-
-
-FLAGS = tf.flags.FLAGS
-FLAGS._parse_flags()
-print("\nParameters:")
-for attr, value in sorted(FLAGS.__flags.items()):
-    print("{}={}".format(attr.upper(), value))
-print("")
-
-vocab_path = os.path.join(FLAGS.checkpoint_dir, "..", "vocab")
-vocab_processor = learn.preprocessing.VocabularyProcessor.restore(vocab_path)
-
-# Evaluation
-# ==================================================
-checkpoint_file = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
-graph = tf.Graph()
-with graph.as_default():
-    session_conf = tf.ConfigProto(
-      allow_soft_placement=FLAGS.allow_soft_placement,
-      log_device_placement=FLAGS.log_device_placement)
-    sess = tf.Session(config=session_conf)
-    with sess.as_default():
-        # Load the saved meta graph and restore variables
-        saver = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
-        saver.restore(sess, checkpoint_file)
-
-        # Get the placeholders from the graph by name
-        input_x = graph.get_operation_by_name("input_x").outputs[0]
-        # input_y = graph.get_operation_by_name("input_y").outputs[0]
-        dropout_keep_prob = graph.get_operation_by_name("dropout_keep_prob").outputs[0]
-
-        # Tensors we want to evaluate
-        predictions = graph.get_operation_by_name("output/predictions").outputs[0]
-
-        # Ask question
-        while True:
-            x_raw = [input('Enter your RT review :> ')]
-            # Map data into vocabulary
-            x_test = np.array(list(vocab_processor.transform(x_raw)))
-            thisPerdiction = sess.run(predictions, {input_x: x_test, dropout_keep_prob: 1.0})
-            for p in thisPerdiction:
-                print("It will be: {} ({})".format("Good" if p == 1.0 else "Rotten", p))
+# Ask questions
+while True:
+    input_list = [input('Enter your RT review :> ')]
+    # Map data into vocabulary
+    predictions = prediction.predict(input_list)
+    for p in predictions:
+        print("It will be: {} ({})".format("Good" if p == 1.0 else "Rotten", p))
